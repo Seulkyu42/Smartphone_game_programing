a39269e5741ba5d1324d3102eb7579dd76d0d7dc
Kyulestial(MSI) <clsrn863@naver.com>
2022-03-14 16:45:51 +0900

    임포트 하기
    Alt + Enter 사용하기
    찾아서 엔터 쳐서 등록(임포트)해주기

--- SGP/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/sgp/MainActivity.java
+++ SGP/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/sgp/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.tukorea.ge.sgp02.s2017180003.sgp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.subText);
     }
 }
