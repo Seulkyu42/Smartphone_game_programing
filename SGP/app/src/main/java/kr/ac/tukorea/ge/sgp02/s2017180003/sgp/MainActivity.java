d137f909f1b5bc41a402232c1f62005c36e1964d
Kyulestial(MSI) <clsrn863@naver.com>
2022-03-14 16:59:19 +0900

    버튼 누를시 텍스트 바꾸기
     btn.setOnClickListener(new View.OnClickListener 여기 클릭되면 호출됨
    
    근데 이 방식은 글이 넘 길어서 잘 안씀

--- SGP/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/sgp/MainActivity.java
+++ SGP/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/sgp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.subText);
         tv.setText("I am a good Programmer");
+
+        Button btn = findViewById(R.id.btnPushMe);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("Clicked !!!");
+            }
+        });
+
     }
 }
