580c47f79d727f6a82c0cbcdd4d35954d1a235cb
Kyulestial(MSI) <clsrn863@naver.com>
2022-03-21 17:21:27 +0900

    alert 호출 다른 방식

--- Cards/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/crads/MainActivity.java
+++ Cards/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/crads/MainActivity.java
@@ -57,18 +57,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void askRetry() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Restart");
-        builder.setMessage("Do you Really want to restart the game?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                startGame();
-            }
-        });
-        builder.setNegativeButton("No",null);
-        AlertDialog dlg = builder.create();
-        dlg.show();
+        new AlertDialog.Builder(this)
+                .setTitle("Restart")
+                .setMessage("Do you Really want to restart the game?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startGame();
+                    }
+                })
+                .setNegativeButton("No",null)
+                .create()
+                .show();
     }
 
     public void onBtnCard(View view) {
