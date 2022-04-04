d90129678e4b75f860892d36968fc393e47d39d1
Kyulestial(MSI) <clsrn863@naver.com>
2022-04-04 16:12:50 +0900

    중요한정보들 업데이트

--- Gameview2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/gameview2/Ball.java
+++ Gameview2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/gameview2/Ball.java
@@ -1,6 +1,7 @@
 package kr.ac.tukorea.ge.sgp02.s2017180003.gameview2;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Rect;
 
 public class Ball {
@@ -9,12 +10,46 @@ public class Ball {
     private Rect dstRect = new Rect();
     private int deltaX,deltaY;
 
-    Ball(int deltaX,int deltaY){
-        // 초기화
+
+    // alt + insert 하면 컨스트럭트 자동으로 만들어줌
+    public Ball(int deltaX, int deltaY) {
         this.deltaX = deltaX;
         this.deltaY = deltaY;
 
         dstRect.set(0,0,200,200);
+    }
 
+    // alt + insert 로 setter
+    public static void setBitmap(Bitmap bitmap) {
+        Ball.bitmap = bitmap;
+        srcRect.set(0,0,bitmap.getWidth(),bitmap.getHeight());
+    }
+
+    public void update() {
+        dstRect.offset(deltaX, deltaY);
+        if (deltaX >= 0) {
+            if (dstRect.right > GameView.view.getWidth()) {
+                deltaX = -deltaX;
+            }
+        }
+        else{
+            if(dstRect.left < 0){
+                deltaX = -deltaX;
+            }
+        }
+        if(deltaY > 0){
+            if(dstRect.bottom > GameView.view.getHeight()){
+                deltaY = -deltaY;
+            }
+        }
+        else{
+            if(dstRect.top <0){
+                deltaY = -deltaY;
+            }
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap,srcRect,dstRect,null);
     }
 }
--- Gameview2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/gameview2/GameView.java
+++ Gameview2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2017180003/gameview2/GameView.java
@@ -15,6 +15,9 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GameView extends View implements Choreographer.FrameCallback {
     private static final String TAG = GameView.class.getSimpleName();
     private final Paint paint = new Paint();
@@ -25,11 +28,16 @@ public class GameView extends View implements Choreographer.FrameCallback {
 //    private int ballDx1,ballDy1;
 //    private int ballDx2,ballDy2;
 
+    private ArrayList<Ball> balls = new ArrayList<>();
 
+    Ball ball1,ball2;
     private Handler handler = new Handler();
     private long previousTime;
     private int framePerSecond; // 멤버변수로 바꾸면 이름길게쓰라
     private Paint fpsPaint = new Paint();
+
+    public static GameView view;
+    private int BALL_COUNT = 10;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -38,19 +46,29 @@ public class GameView extends View implements Choreographer.FrameCallback {
     }
 
     private void initView() {
+        view = this;
+
         fpsPaint.setTextSize(100);
         paint.setColor(0xFFCCCCCC);
         Resources res = getResources();
-        soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
-        soccerSrcRect.set(0, 0, soccerBitmap.getWidth(), soccerBitmap.getHeight());
-        soccerDstRect1.set(0,0,100,100);
-        soccerDstRect2.set(0,0,100,100);
+        Bitmap soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+        Ball.setBitmap(soccerBitmap);
+//        soccerSrcRect.set(0, 0, soccerBitmap.getWidth(), soccerBitmap.getHeight());
 
-        ballDx1 = 10;
-        ballDy1 = 10;
+        Random random = new Random();
+        for(int i =0;i < BALL_COUNT; ++i){
+            int dx = random.nextInt(10)+5; // 0~9 + 5 값 임
+            int dy = random.nextInt(10)+5;
+        }
+        Ball ball1 = new Ball(10,10);
+        Ball ball2 = new Ball(15,20);
 
-        ballDx2 = 7;
-        ballDy2 = 15;
+        balls.add(ball1);
+        balls.add(ball2);
+
+//        soccerDstRect1.set(0,0,100,100);
+//        ballDx1 = 10;
+//        ballDy1 = 10;
 
         Choreographer.getInstance().postFrameCallback(this);
         //updateFrame();
@@ -79,56 +97,68 @@ public class GameView extends View implements Choreographer.FrameCallback {
     }
 
     private void update() {
-        soccerDstRect1.offset(ballDx1, ballDy1);
-        if (ballDx1 >= 0) {
-            if (soccerDstRect1.right > getWidth()) {
-                ballDx1 = -ballDx1;
-            }
+//        ball1.update();
+//        ball2.update();
+        for(Ball ball : balls){
+            ball.update();
         }
-        else{
-            if(soccerDstRect1.left < 0){
-                ballDx1 = -ballDx1;
-            }
-        }
-        if(ballDy1 > 0){
-            if(soccerDstRect1.bottom > getHeight()){
-                ballDy1 = -ballDy1;
-            }
-        }
-        else{
-            if(soccerDstRect1.top <0){
-                ballDy1 = -ballDy1;
-            }
-        }
+//        soccerDstRect1.offset(ballDx1, ballDy1);
+//        if (ballDx1 >= 0) {
+//            if (soccerDstRect1.right > getWidth()) {
+//                ballDx1 = -ballDx1;
+//            }
+//        }
+//        else{
+//            if(soccerDstRect1.left < 0){
+//                ballDx1 = -ballDx1;
+//            }
+//        }
+//        if(ballDy1 > 0){
+//            if(soccerDstRect1.bottom > getHeight()){
+//                ballDy1 = -ballDy1;
+//            }
+//        }
+//        else{
+//            if(soccerDstRect1.top <0){
+//                ballDy1 = -ballDy1;
+//            }
+//        }
 
-        soccerDstRect2.offset(ballDx2, ballDy2);
-        if (ballDx2 >= 0) {
-            if (soccerDstRect2.right > getWidth()) {
-                ballDx2 = -ballDx2;
-            }
-        }
-        else{
-            if(soccerDstRect2.left < 0){
-                ballDx2 = -ballDx2;
-            }
-        }
-        if(ballDy2 > 0){
-            if(soccerDstRect2.bottom > getHeight()){
-                ballDy2 = -ballDy2;
-            }
-        }
-        else{
-            if(soccerDstRect2.top <0){
-                ballDy2 = -ballDy2;
-            }
-        }
+//        soccerDstRect2.offset(ballDx2, ballDy2);
+//        if (ballDx2 >= 0) {
+//            if (soccerDstRect2.right > getWidth()) {
+//                ballDx2 = -ballDx2;
+//            }
+//        }
+//        else{
+//            if(soccerDstRect2.left < 0){
+//                ballDx2 = -ballDx2;
+//            }
+//        }
+//        if(ballDy2 > 0){
+//            if(soccerDstRect2.bottom > getHeight()){
+//                ballDy2 = -ballDy2;
+//            }
+//        }
+//        else{
+//            if(soccerDstRect2.top <0){
+//                ballDy2 = -ballDy2;
+//            }
+//        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawText("Fps :"+framePerSecond,framePerSecond*10,100,fpsPaint);
-        canvas.drawBitmap(soccerBitmap, soccerSrcRect, soccerDstRect1, null); // 비트맵은 null도 넣어도됨
-        canvas.drawBitmap(soccerBitmap, soccerSrcRect, soccerDstRect2, null); // 비트맵은 null도 넣어도됨
+        for(Ball ball : balls){
+            ball.draw(canvas);
+        }
+
+        //ball1.draw(canvas);
+        //ball2.draw(canvas);
+//        canvas.drawBitmap(Ball.bitmap, Ball.srcRect, Ball.dstRect, null);
+//        canvas.drawBitmap(soccerBitmap, soccerSrcRect, soccerDstRect1, null); // 비트맵은 null도 넣어도됨
+//        canvas.drawBitmap(soccerBitmap, soccerSrcRect, soccerDstRect2, null); // 비트맵은 null도 넣어도됨
     }
 
 
