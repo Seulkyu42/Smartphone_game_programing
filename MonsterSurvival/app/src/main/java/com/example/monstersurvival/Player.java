5a6ba80f0ba7cc64eb037abc10bb6d0d24190fd2
Mingo <clsrn863@naver.com>
2022-04-14 10:30:30 +0900

    MS / 플레이어 클래스 좌표및 체력,스피드 설정
    MonsterSurvival

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/Player.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/Player.java
@@ -1,5 +1,18 @@
 package com.example.monstersurvival;
 
 public class Player {
+    private float px; //Player X
+    private float py; //Player Y
+
+    private float speed;
+    private int health;
+
+
+    // init
+    public Player(float x,float y){
+        speed = 10; //dimen에 넣을예정
+        health = 3;
+    }
+
 
 }
