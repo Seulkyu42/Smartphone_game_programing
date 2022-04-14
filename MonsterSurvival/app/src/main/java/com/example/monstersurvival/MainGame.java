7b95098f0b2866917560e851bc5f4e9d90d82284
Mingo <clsrn863@naver.com>
2022-04-14 11:02:22 +0900

    MS 메인게임 클래스 등록

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/MainGame.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/MainGame.java
@@ -1,2 +1,36 @@
-package com.example.monstersurvival;public class MainGame {
+package com.example.monstersurvival;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class MainGame {
+
+    public static MainGame getInstance() {
+        if (singleton == null) {
+            singleton = new MainGame();
+        }
+        return singleton;
+    }
+
+    public float frameTime;
+
+    private MainGame(){
+    }
+
+    private static MainGame singleton;
+    private Player player;
+
+    private void init(){
+
+    }
+    private void update(){
+
+    }
+
+    private void draw(Canvas canvas){
+
+    }
+
+
+
 }
