4d8c454838bc72f99656d479035a855b4882b278
Mingo <clsrn863@naver.com>
2022-04-14 10:35:28 +0900

    MS / 플레이어 재화 등록

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/Player.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/Player.java
@@ -12,13 +12,14 @@ public class Player {
     private float px; //Player X
     private float py; //Player Y
 
+    // 플레이어 스텟 및 재화
     private float speed;
     private int health;
-
-
+    private int coin;
+ 
     // init
     public Player(float x,float y){
-
+        coin = 0;
         speed = 10; //dimen에 넣을예정
         health = 3;
     }
