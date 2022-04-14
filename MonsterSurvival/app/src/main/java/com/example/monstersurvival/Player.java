89b396c7fada74e83fc29ab297f6fcd46db2fe8a
Mingo <clsrn863@naver.com>
2022-04-14 10:34:25 +0900

    MS / 플레이어 나머지 함수 등록

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/Player.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/Player.java
@@ -1,6 +1,14 @@
 package com.example.monstersurvival;
 
+
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+import android.graphics.Canvas;
+
 public class Player {
+    private Bitmap playerBitmap;
+    private RectF playerRect = new RectF();
+
     private float px; //Player X
     private float py; //Player Y
 
@@ -10,9 +18,17 @@ public class Player {
 
     // init
     public Player(float x,float y){
+
         speed = 10; //dimen에 넣을예정
         health = 3;
     }
 
+    public void update(){
+
+    }
+
+    public void draw(Canvas canvas){
+        //canvas.drawBitmap(playerBitmap,);
+    }
 
 }
