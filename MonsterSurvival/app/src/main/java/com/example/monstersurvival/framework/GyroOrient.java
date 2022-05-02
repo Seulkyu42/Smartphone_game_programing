012b380c1dc23a96ba9b12dd2df4d284fe4870c1
Mingo <clsrn863@naver.com>
2022-05-02 10:31:09 +0900

    MS /  자이로 새로운 방식 (2/??)

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/framework/GyroOrient.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/framework/GyroOrient.java
@@ -1,27 +1,71 @@
 package com.example.monstersurvival.framework;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.provider.SyncStateContract;
 
 public class GyroOrient implements SensorEventListener {
     private SensorManager manager;
-    private Sensor accelermeter;
+    private Sensor accelerometer;
     private Sensor magnometer;
 
     private float[] accelOutput;
+    private float[] magOutput;
+
+    private float[] orientation = new float[3];
     public float[] getOrientation(){
         return orientation;
     }
 
+    private float[] startOrientation = null;
+    public float[] getStartOrientation() {
+        return startOrientation;
+    }
+    public void newGame() {
+        startOrientation = null;
+    }
+
+    public GyroOrient(){
+        manager = (SensorManager)Constants.CURRENT_CONTEXT.getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnometer = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+
+    public void register() {
+        manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        manager.registerListener(this, magnometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    public void pause() {
+        manager.unregisterListener(this);
+    }
+
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            accelOutput = event.values;
+        else if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            magOutput = event.values;
+        if(accelOutput != null && magOutput != null) {
+            float[] R = new float[9];
+            float[] I = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, accelOutput, magOutput);
+            if(success) {
+                SensorManager.getOrientation(R, orientation);
+                if(startOrientation == null) {
+                    startOrientation = new float[orientation.length];
+                    System.arraycopy(orientation, 0, startOrientation, 0, orientation.length);
+                }
+            }
+        }
     }
 }
