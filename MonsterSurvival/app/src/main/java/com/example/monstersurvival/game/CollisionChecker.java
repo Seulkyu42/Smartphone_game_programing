e5fd828fc46fb45de303e066dfd079e5c439c159
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2022-05-19 19:11:39 +0900

    MS / 적-플레이어 충돌체크

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/game/CollisionChecker.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/game/CollisionChecker.java
@@ -2,8 +2,10 @@ package com.example.monstersurvival.game;
 
 
 import android.graphics.Canvas;
+import android.util.Log;
 
 import com.example.monstersurvival.framework.BoxCollidable;
+import com.example.monstersurvival.framework.CollisionHelper;
 import com.example.monstersurvival.framework.GameObject;
 
 import java.util.ArrayList;
@@ -19,8 +21,16 @@ public class CollisionChecker implements GameObject {
     @Override
     public void update() {
         MainGame game = MainGame.getInstance();
-//        Player player = (Player) game.objectsAt(MainGame.Layer.player.ordinal()).get(0);
-        //<GameObject> items = game.objectsAt(MainGame.Layer.item.ordinal());
+        ArrayList<GameObject> enemies = game.objectsAt(MainGame.Layer.enemy);
+
+        for(GameObject enemy: enemies){
+            if(!(enemy instanceof BoxCollidable)){
+                continue;
+            }
+            if(CollisionHelper.collides(player,(BoxCollidable) enemy)){
+                Log.d(TAG, "Collision: " + enemy);
+            }
+        }
 //        for (GameObject item: items) {
 //            if (!(item instanceof BoxCollidable)) {
 //                continue;
