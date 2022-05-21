ec6ed31a962942f9c777192f64e6a8263fc4aa77
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2022-05-21 22:46:58 +0900

    MS / 적 클래스를 이용해서 아이템 만들기

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/game/Item1.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/game/Item1.java
@@ -3,10 +3,8 @@ package com.example.monstersurvival.game;
 import android.graphics.Canvas;
 import android.graphics.PointF;
 import android.graphics.RectF;
-import android.util.Log;
 
 import com.example.monstersurvival.R;
-import com.example.monstersurvival.framework.AnimSprite;
 import com.example.monstersurvival.framework.BitmapPool;
 import com.example.monstersurvival.framework.BoxCollidable;
 import com.example.monstersurvival.framework.Metrics;
@@ -15,9 +13,9 @@ import com.example.monstersurvival.framework.RecycleBin;
 import com.example.monstersurvival.framework.Sprite;
 
 
-public class Enemy extends Sprite implements BoxCollidable, Recyclable {
+public class Item1 extends Sprite implements BoxCollidable, Recyclable {
     public static final float FRAMES_PER_SECOND = 10.0f;
-    private static final String TAG = Enemy.class.getSimpleName();
+    private static final String TAG = Item1.class.getSimpleName();
     public static float size;
     protected float dy;
     protected RectF boundingBox = new RectF();
@@ -31,13 +29,13 @@ public class Enemy extends Sprite implements BoxCollidable, Recyclable {
 
 
 //    protected static ArrayList<Enemy> recycleBin = new ArrayList<>();
-    public static Enemy get(float x, float speed) {
-        Enemy enemy = (Enemy) RecycleBin.get(Enemy.class);
+    public static Item1 get(float x, float speed) {
+        Item1 enemy = (Item1) RecycleBin.get(Item1.class);
         if (enemy != null) {
             enemy.set(x, speed);
             return enemy;
         }
-        return new Enemy(x, speed);
+        return new Item1(x, speed);
     }
 
     private void set(float x, float speed) {
@@ -53,7 +51,7 @@ public class Enemy extends Sprite implements BoxCollidable, Recyclable {
         this.player = player;
     }
 
-    private Enemy(float x, float speed) {
+    private Item1(float x, float speed) {
         super(x,size,R.dimen.player_radius, R.mipmap.enemy_image);
         set(x,speed);
         dy = speed;
@@ -62,7 +60,6 @@ public class Enemy extends Sprite implements BoxCollidable, Recyclable {
     @Override
     public void update() {
         objPosition = player.getCurrPosition();
-        float dx = objPosition.x;
 
         if (life <= 0) {
             //MainGame.getInstance().remove(MainGame.Layer.enemy);
@@ -71,13 +68,6 @@ public class Enemy extends Sprite implements BoxCollidable, Recyclable {
 
         float frameTime = MainGame.getInstance().frameTime;
 
-        if(y <= Metrics.height / 32) {
-            if (dx >= x) {
-                x += 1 + dx * frameTime;
-            } else {
-                x -= 1 + dx * frameTime;
-            }
-        }
         y += dy * frameTime;
         setDstRectWithRadius();
         boundingBox.set(dstRect);
