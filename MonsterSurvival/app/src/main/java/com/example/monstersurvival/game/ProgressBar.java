9c9c06b53f96b829b9171cbadb68ac09c651f1d9
Mingo <clsrn863@naver.com>
2022-05-31 16:41:57 +0900

    MS / 진행중

--- MonsterSurvival/app/src/main/java/com/example/monstersurvival/game/ProgressBar.java
+++ MonsterSurvival/app/src/main/java/com/example/monstersurvival/game/ProgressBar.java
@@ -7,6 +7,7 @@ import com.example.monstersurvival.R;
 import com.example.monstersurvival.framework.GameView;
 import com.example.monstersurvival.framework.interfaces.GameObject;
 import com.example.monstersurvival.framework.objects.Sprite;
+import com.example.monstersurvival.framework.res.Metrics;
 
 public class ProgressBar implements GameObject {
 
@@ -14,18 +15,28 @@ public class ProgressBar implements GameObject {
     protected RectF lifeRect = new RectF();
     //protected Bitmap bitmap;
     private Player player;
+    protected Gauge gauge;
 
     public void setPlayer(Player player) {
         this.player = player;
     }
-    
+
+    private ProgressBar(){
+        gauge = new Gauge(
+                Metrics.size(R.dimen.enemy_gauge_fg_width), R.color.enemy_gauge_fg,
+                Metrics.size(R.dimen.enemy_gauge_bg_width), R.color.enemy_gauge_bg,
+                size * 0.9f
+        );
+        gauge.setValue(1.0f);
+    }
+
     @Override
     public void update() {
 
     }
 
     public void draw(Canvas canvas){
-
+        gauge.draw(canvas, 50, 50 + size * 0.5f);
     }
 
 
